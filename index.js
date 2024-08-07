23f1a376b7d3efd4e0f7769c439396570112f286
Hukam Singh Saini <hukamsaini43644@gmail.com>
2024-08-07 22:48:01 +0530

    minar change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 const freecurrencyapi = new Freecurrencyapi('fca_live_q0vdCfeDgv3BRTGxEAxdoClxb8vnKQFvvypiLHDa');
 
 
-convertCurrency("USD", "INR", 3);
+
 
 export async function convertCurrency(fromCurrency, toCurrency, units){
     const res = await freecurrencyapi.latest({
